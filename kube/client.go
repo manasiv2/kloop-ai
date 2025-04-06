e5fb744f444368fc9dbc3d31bb20e3c4226db44e
Manasi Vaidya <manasi.vaidya@senseonics.com>
2025-04-05 20:04:20 -0500

    4 functions to detect sus pods based on pod status, events, conditions, and container. current map[podUID] -> description of issue future -> PodErrorMetadata

--- kube/client.go
+++ kube/client.go
@@ -2,6 +2,7 @@ package kube
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"path"
 
@@ -11,18 +12,20 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
+func getClient() (*kubernetes.Clientset, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+	config, err := clientcmd.BuildConfigFromFlags("", path.Join(home, ".kube/config"))
+	if err != nil {
+		return nil, err
+	}
+	return kubernetes.NewForConfig(config)
+}
+
 func getPods(namespace string) (*corev1.PodList, error) {
-	home, err := os.UserHomeDir() // get home dir path
-	if err != nil {
-		panic(err)
-	}
-
-	config, err := clientcmd.BuildConfigFromFlags("", path.Join(home, ".kube/config")) // add .kube/config to path
-	if err != nil {
-		panic(err.Error())
-	}
-
-	client, err := kubernetes.NewForConfig(config) // call to k8s
+	client, err := getClient() // call to k8s
 	if err != nil {
 		panic(err.Error())
 	}
@@ -44,7 +47,7 @@ func getPods(namespace string) (*corev1.PodList, error) {
 	return podlist, nil
 }
 
-func CheckPodPhases(namespace string) []PodErrorMetadata {
+/*func CheckPodPhases(namespace string) []PodErrorMetadata {
 	pods, err := getPods(namespace)
 	if err != nil {
 		panic(err)
@@ -77,9 +80,94 @@ func CheckPodPhases(namespace string) []PodErrorMetadata {
 		}
 	}
 	return errors
+}*/
+
+func GetSuspiciousFromPhase(namespace string) map[string]string {
+	pods, err := getPods(namespace)
+	if err != nil {
+		panic(err)
+	}
+	suspiciousPods := make(map[string]string)
+	for _, pod := range pods.Items {
+		pod_phase := pod.Status.Phase
+		if pod_phase != corev1.PodRunning && pod_phase != corev1.PodSucceeded {
+			suspiciousPods[string(pod.UID)] = fmt.Sprintf("Pod Phase Issue: %s", pod_phase)
+		}
+	}
+	return suspiciousPods
 }
 
-//func checkPodConditions(...) []PodErrorMetadata
-//func checkPodEvents(...) []PodErrorMetadata
-//func checkContainerStatus(namespace string) []PodErrorMetadata
-//func sendToLLM([]PodErrorMetadata) string
+func GetSuspiciousFromContainerStatus(namespace string) map[string]string {
+	pods, err := getPods(namespace)
+	if err != nil {
+		panic(err)
+	}
+	suspiciousPods := make(map[string]string)
+	for _, pod := range pods.Items {
+		for _, container := range pod.Status.ContainerStatuses {
+			if container.State.Waiting != nil {
+				if container.State.Waiting.Reason == "CrashLoopBackOff" || container.State.Waiting.Reason == "ImagePullBackOff" || container.State.Waiting.Reason == "ErrImagePull" {
+					suspiciousPods[string(pod.UID)] = fmt.Sprintf("Container Status Issue: %s", container.State.Waiting.Reason)
+				}
+			} else if container.State.Terminated != nil {
+				if container.State.Terminated.ExitCode != 0 {
+					suspiciousPods[string(pod.UID)] = fmt.Sprintf("Container Status Issue: %s", container.State.Terminated.Reason)
+				}
+			} else {
+				// init containers -> can check later
+			}
+		}
+	}
+	return suspiciousPods
+}
+
+func GetSuspiciousFromConditions(namespace string) map[string]string {
+	pods, err := getPods(namespace)
+	if err != nil {
+		panic(err)
+	}
+	suspiciousPods := make(map[string]string)
+	for _, pod := range pods.Items {
+		for _, condition := range pod.Status.Conditions {
+			if condition.Type == corev1.PodScheduled && condition.Status == corev1.ConditionFalse && condition.Reason == "Unschedulable" {
+				suspiciousPods[string(pod.UID)] = fmt.Sprintf("Pod Condition Issue: %s, %s", condition.Type, condition.Reason)
+			} else if condition.Type == corev1.PodReady || condition.Type == corev1.ContainersReady {
+				if condition.Status == corev1.ConditionFalse || condition.Status == corev1.ConditionUnknown {
+					suspiciousPods[string(pod.UID)] = fmt.Sprintf("Pod Condition Issue: %s, %s", condition.Type, condition.Reason)
+				}
+			}
+		}
+	}
+	return suspiciousPods
+}
+
+func GetSuspiciousFromEvents(namespace string) map[string]string {
+	client, err := getClient()
+	if err != nil {
+		panic(err.Error())
+	}
+	pods, err := getPods(namespace)
+	if err != nil {
+		panic(err)
+	}
+	suspiciousPods := make(map[string]string)
+	for _, pod := range pods.Items {
+		pod_uid := pod.UID
+		events, er := client.CoreV1().Events(namespace).List(
+			context.TODO(),
+			metav1.ListOptions{
+				FieldSelector: fmt.Sprintf("involvedObject.uid=%s", pod_uid),
+			},
+		)
+		if er != nil {
+			panic(er)
+		}
+		for _, event := range events.Items {
+			if event.Type == corev1.EventTypeWarning && (event.Reason == "FailedScheduling" || event.Reason == "BackOff" ||
+				event.Reason == "FailedMount" || event.Reason == "Unhealthy" || event.Reason == "FailedCreatePodSandBox" || event.Reason == "ErrImagePull" || event.Reason == "ImagePullBackOff") {
+				suspiciousPods[string(pod.UID)] = fmt.Sprintf("Event Issue: %s, %s", event.Type, event.Reason)
+			}
+		}
+	}
+	return suspiciousPods
+}
